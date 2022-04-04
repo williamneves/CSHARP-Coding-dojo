56a7e55badfb45a29073481cce85f3c3466909fd
William Neves <drwilliamneves@gmail.com>
2022-04-03 20:29:10 -0400

    Loops C#

---
+++ FirstCSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FirstCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                int randomNumber = rnd.Next(1, 10);
+                Console.WriteLine(randomNumber);
+            }
+        }
+
+
+    }
+}
---
+++ Fundamentals_I/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Fundamentals_I
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string hello = "Assigment Fundamentals I";
+
+            Console.WriteLine(hello);
+
+            Console.ReadLine();
+
+            Console.WriteLine("All from 1 to 255");
+            for(int i = 1; i <= 255; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Buzzfizz");
+            for(int i = 1; i <= 100; i++)
+            {
+                if(i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if(i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if(i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.WriteLine("Now with with while loop");
+            int j = 1;
+            
+            while(j < 256){
+
+                Console.WriteLine(j);
+                j++;
+            }
+
+            int t = 1;
+
+            while(t < 101){
+                if(t % 3 == 0 && t % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if(t % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if(t % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(t);
+                }
+                t++;
+            }
+        }
+    }
+}
