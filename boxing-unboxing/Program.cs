8d761838c78eaa0c574bc0d7eb6050ff6624ee63
William Neves <drwilliamneves@gmail.com>
2022-04-04 00:07:30 -0400

    boxing

---
+++ boxing-unboxing/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace boxing_unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> list = new List<object>();
+
+            list.Add(7);
+            list.Add(28);
+            list.Add(-1);
+            list.Add(true);
+            list.Add("chair");
+
+            var sum = 0;
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+                if (item is int)
+                {
+                    sum += (int)item;
+                }
+
+            }
+            Console.WriteLine($"sum: {sum}");
+        }
+    }
+}
