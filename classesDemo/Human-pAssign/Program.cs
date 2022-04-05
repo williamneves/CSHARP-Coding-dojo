e6fc11db75f99b3bd557bc968304fe62231e8bea
William Neves <drwilliamneves@gmail.com>
2022-04-05 13:05:18 -0400

    human-assign

---
+++ classesDemo/Human-pAssign/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Human_pAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var hulk = new Human("Hulk");
+            var thor = new Human("Thor");
+
+            hulk.Attack(thor);
+            thor.Attack(hulk);
+            thor.Attack(hulk);
+            System.Console.WriteLine(thor.Health);
+            System.Console.WriteLine(hulk.Health);
+
+        }
+    }
+}
---
+++ classesDemo/Human-pAssign/human.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+
+namespace Human_pAssign
+{
+    public class Human
+    {
+        public string name;
+        public int strength;
+        public int intelligence;
+        public int dexterity;
+        private int health;
+        public int Health { get { return health; } }
+
+        public Human(string name)
+        {
+            this.name = name;
+            strength = 3;
+            intelligence = 3;
+            dexterity = 3;
+            health = 100;
+        }
+
+        public Human(string name, int strength, int intelligence, int dexterity, int health)
+        {
+            this.name = name;
+            this.strength = strength;
+            this.intelligence = intelligence;
+            this.dexterity = dexterity;
+            this.health = health;
+        }
+
+        public int Attack(Human target)
+        {
+            target.health -= 5 * strength;
+            System.Console.WriteLine($"{name} attacked {target.name} for {5 * strength} damage! Now {target.name} has {target.health} health left.");
+            return target.health;
+        }
+    }
+}
