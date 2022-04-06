3dc198b4e289fb5033b5e3cecf945098761b3718
gwilliamnn <97652695+gwilliamnn@users.noreply.github.com>
2022-04-06 08:16:44 -0400

    wizard

---
+++ Wizard_Ninja_Samurai_cAssign/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Wizard_Ninja_Samurai_cAssign
+{
+    class Program
+    {
+static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var hulk = new Human("Hulk");
+            var thor = new Ninja("Thor");
+
+            hulk.Attack(thor);
+            thor.Attack(hulk);
+            thor.Attack(hulk);
+            // System.Console.WriteLine(thor.Health);
+            // System.Console.WriteLine(hulk.Health);
+
+        }
+    }
+}
