d4f70e07b83605f8fb50975d6abe8afaf5dc5ae0
William Neves <drwilliamneves@gmail.com>
2022-04-18 17:58:42 -0400

    TheWall

---
+++ EntityFrameWork/TheWallAssignment/TheWallAssignment/Models/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TheWallAssignment.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int CommentId { get; set; }
+        [Required]
+        public string CommentText { get; set; }
+        public int UserId { get; set; }
+        public int MessageId { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+        public User User { get; set; }
+        public Message Message { get; set; }
+    }
+}
---
+++ EntityFrameWork/TheWallAssignment/TheWallAssignment/Models/Message.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TheWallAssignment.Models
+{
+    public class Message
+    {
+        [Key]
+        public int MessageId { get; set; }
+        public string MessageText { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        
+        // Relationships
+        // Each message has a user
+        public int UserId { get; set; }
+        public User User { get; set; }
+        
+        // 1 Message to many Comments
+        public List<Comment> Comments { get; set; }
+    }
+}
